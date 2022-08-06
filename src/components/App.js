68a1adba07d75289326a21b887385fa8d4e1d05e
Enzo <enzo202p@gmail.com>
2022-08-06 02:00:43 -0300

    Correção de um bug critico

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import LoginPage from "./LoginPage";
 import SingIn from "./SingIn";
 import Habits from "./Habits";
+import {UserContext} from "../contexts/UserContext"
 import '../theme/reset.css'
 
 export default function App(){
+    const [teste,setTeste] = useState('');
     return(
         <BrowserRouter>
             <Routes>
